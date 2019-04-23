28831e32ec12214ceaceb54d9bd5c5c7003c118c
Will Fritts <waf515@gmail.com>
2019-04-22 18:45:32 -0700

    completed challenge 2

--- coding-challenges/for-each/challenges-01.test.js
+++ coding-challenges/for-each/challenges-01.test.js
@@ -44,7 +44,7 @@ const addValues = (arr, value) => {
 const addNumbers = (num, arr, times, callback) => {
   // Solution code here...
   for(let i = 0; i < times; i++){
-    callback(addValues(arr,num));
+    callback(arr,num);
   }
   return arr;
 };
