aa536e17b3189ade7da79e3eaf20f92457268e1f
Will Fritts <waf515@gmail.com>
2019-04-22 18:51:06 -0700

    completed challenge 3

--- coding-challenges/for-each/challenges-01.test.js
+++ coding-challenges/for-each/challenges-01.test.js
@@ -63,10 +63,17 @@ Return the modified array.
 
 const removeOne = (num, arr) => {
   // Solution code here...
+  if(num % 3 === 2){
+    arr.pop();
+  }
 };
 
 const removeElements = (arr, callback) => {
   // Solution code here...
+  for(let i = 0; i < arr.length; i++){
+    callback(arr[i], arr);
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
