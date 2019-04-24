e3ec149ed9e512e7cb91329a3cc8ac574c33be34
Will Fritts <waf515@gmail.com>
2019-04-23 17:21:29 -0700

    completed challenge 3

--- coding-challenges/value-vs-reference/challenge-02.test.js
+++ coding-challenges/value-vs-reference/challenge-02.test.js
@@ -54,6 +54,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
+  obj.yearBorn = year;
 };
 
 /* ------------------------------------------------------------------------------------------------
