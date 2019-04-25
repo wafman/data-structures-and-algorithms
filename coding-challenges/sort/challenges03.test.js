26394e9c23229a2fe0b3925d865ebc004795f9f8
Will Fritts <waf515@gmail.com>
2019-04-24 17:22:04 -0700

    finished challenge 5

--- coding-challenges/sort/challenges03.test.js
+++ coding-challenges/sort/challenges03.test.js
@@ -100,6 +100,8 @@ Here is an example of the input:
 
 const sortByPrice = (arr) => {
   // Solution code here...
+  arr.sort((a,b)=> a.price - b.price);
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
