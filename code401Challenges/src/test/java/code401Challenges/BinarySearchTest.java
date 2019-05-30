7d2c14452a4e3a39cc75caceb2e7bbd8860afef4
Will Fritts <waf515@gmail.com>
2019-05-30 08:24:12 -0700

    completed tests for code, all pass

--- code401Challenges/src/test/java/code401Challenges/BinarySearchTest.java
+++ code401Challenges/src/test/java/code401Challenges/BinarySearchTest.java
@@ -1,4 +1,28 @@
 package code401Challenges;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class BinarySearchTest {
+    @Test
+    public void testBinarySearch_present(){
+        BinarySearch classUnderTest = new BinarySearch();
+        int num = 15;
+        int[] arr = new int[]{4,8,15,16,23,42};
+        int expectedOutput = 2;
+        assertEquals("binary search should return index 2 where number 15 is located",
+                expectedOutput,
+                classUnderTest.binarySearch(arr,num));
+    }
+
+    @Test
+    public void testBinarySearch_notPresent(){
+        BinarySearch classUnderTest = new BinarySearch();
+        int num = 90;
+        int[] arr = new int[]{11,22,33,44,55,66,77};
+        int expectedOutput = -1;
+        assertEquals("binary search should return -1 when number is not in array",
+                expectedOutput,
+                classUnderTest.binarySearch(arr, num));
+    }
 }
