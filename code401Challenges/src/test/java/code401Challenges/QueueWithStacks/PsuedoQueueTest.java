ea0930ee38b81d47fc6725a5ad06aed532f65ce6
Will Fritts <waf515@gmail.com>
2019-06-10 12:25:48 -0700

    testing completed for enqueue, dequeue

--- code401Challenges/src/test/java/code401Challenges/QueueWithStacks/PsuedoQueueTest.java
+++ code401Challenges/src/test/java/code401Challenges/QueueWithStacks/PsuedoQueueTest.java
@@ -20,13 +20,27 @@ public class PsuedoQueueTest {
         assertTrue("should return 5",  p.stackOne.peek() == 5);
     }
 
-//    @Test
-//    public void testEnqueue_multipleValues(){
-//        PsuedoQueue p = new PsuedoQueue();
-//        p.enqueue(5);
-//        p.enqueue(10);
-//        p.enqueue(15);
-//        assertTrue("should return 3", );
-//    }
+    @Test
+    public void testDequeue_singleValue(){
+        PsuedoQueue p = new PsuedoQueue();
+        p.enqueue(5);
+        assertTrue("should return null", p.dequeue() == 5);
+    }
+
+    @Test
+    public void testEnqueue_and_deque_multipleValues(){
+        PsuedoQueue p = new PsuedoQueue();
+        p.enqueue(5);
+        p.enqueue(10);
+        p.enqueue(15);
+        p.enqueue(20);
+        assertTrue("should return 5", p.dequeue() == 5 );
+        assertTrue("should return 10", p.dequeue() == 10);
+        assertTrue("should return 15", p.dequeue() == 15);
+        assertTrue("should return 20l", p.dequeue() == 20);
+        assertTrue("should return null", p.dequeue() == null);
+    }
+
+
 
 }
