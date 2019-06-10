94f45d329df3c5fe10550fb53dc537e43f53586f
Will Fritts <waf515@gmail.com>
2019-06-10 08:21:36 -0700

    queue feature and testing done

--- code401Challenges/src/main/java/code401Challenges/stacksandqueues/Queue.java
+++ code401Challenges/src/main/java/code401Challenges/stacksandqueues/Queue.java
@@ -1,4 +1,64 @@
 package code401Challenges.stacksandqueues;
 
 public class Queue {
+    private Node front;
+    private Node last;
+    private int count;
+
+    public Queue(){
+        this.front = null;
+        this.last = null;
+        this.count = 0;
+    }
+
+    //getters
+
+    public Node getFront() {
+        return front;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+
+    //helper functions
+    public boolean isEmpty(){
+        return front == null;
+    }
+
+
+    //feature functions
+
+    public void enqueue(int data){
+        Node n = new Node(data);
+        if(isEmpty() == true){
+            front = n;
+            last = n;
+            count++;
+        } else {
+            last.next = n;
+            last = n;
+            count++;
+        }
+    }
+
+    public int dequeue(){
+        if(isEmpty() == true){
+            System.out.println("Queue is empty");
+        }
+        int value = front.value;
+        front = front.next;
+        count--;
+        return value;
+    }
+
+    public int peek(){
+        if(isEmpty() == true){
+            System.out.println("Queue is empty");
+        }
+        return front.value;
+    }
+
+
 }
--- code401Challenges/src/test/java/code401Challenges/stacksandqueues/QueueTest.java
+++ code401Challenges/src/test/java/code401Challenges/stacksandqueues/QueueTest.java
@@ -1,7 +1,60 @@
 package code401Challenges.stacksandqueues;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class QueueTest {
+
+    @Test
+    public void testIsEmpty(){
+        Queue queue = new Queue();
+        assertTrue("should return true", queue.isEmpty());
+    }
+
+    @Test
+    public void testEnqueue_singleValue(){
+        Queue queue = new Queue();
+        queue.enqueue(5);
+        assertTrue("should return 5", queue.getFront().value == 5);
+    }
+
+    @Test
+    public void testEnqueue_multipleValues(){
+        Queue queue = new Queue();
+        queue.enqueue(5);
+        queue.enqueue(10);
+        queue.enqueue(15);
+        assertTrue("size should be 3", queue.getCount() == 3);
+    }
+
+    @Test
+    public void testDequeue_singleValue(){
+        Queue queue = new Queue();
+        queue.enqueue(5);
+        queue.enqueue(10);
+        queue.enqueue(15);
+        assertEquals("should return 10", 5, queue.dequeue());
+    }
+
+    @Test
+    public void testDequeue_multipleValues(){
+        Queue queue = new Queue();
+        queue.enqueue(5);
+        queue.enqueue(10);
+        queue.enqueue(15);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+        assertTrue("should return truee", queue.isEmpty());
+    }
+
+    @Test
+    public void testPeek(){
+        Queue queue = new Queue();
+        queue.enqueue(5);
+        queue.enqueue(10);
+        assertEquals("should return 5", 5, queue.peek());
+    }
 
 }
