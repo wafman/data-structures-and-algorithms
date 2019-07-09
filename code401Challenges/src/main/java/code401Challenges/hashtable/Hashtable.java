7cb4d504d8e53f0c801ae0c15c15f85556300bbc
Will Fritts <waf515@gmail.com>
2019-07-09 07:26:57 -0700

    first two features done

--- code401Challenges/src/main/java/code401Challenges/hashtable/Hashtable.java
+++ code401Challenges/src/main/java/code401Challenges/hashtable/Hashtable.java
@@ -1,4 +1,45 @@
 package code401Challenges.hashtable;
 
+
 public class Hashtable {
+
+    private Node[] map;
+
+
+    //constructor
+    public Hashtable(int size){
+        map = new Node[size];
+    }
+
+    //methods
+    public int hash(String key){
+        int hashValue = 0;
+        char[] words = key.toCharArray();
+        for(int i = 0; i < words.length; i++){
+            hashValue += words[i];
+        }
+        hashValue = (hashValue * 1069) % map.length;
+        return hashValue;
+    }
+
+    public void add(String key, String value){
+        Node node = new Node(key,value);
+        int hashKey = hash(key);
+        if(this.map[hashKey] == null){
+            map[hashKey] = node;
+        }
+        else {
+            Node temp = map[hashKey];
+            map[hashKey] = node;
+            map[hashKey].setNext(temp);
+        }
+    }
+
+    public void get(){
+
+    }
+
+    public void contains(){
+
+    }
 }
