ae280f0347f5b49932ac6edc0141b0e88a91ae67
Will Fritts <waf515@gmail.com>
2019-07-10 10:10:12 -0700

    feature done

---
+++ code401Challenges/src/main/java/code401Challenges/RepeatedWord/RepeatedWord.java
@@ -0,0 +1,30 @@
+package code401Challenges.RepeatedWord;
+
+
+import code401Challenges.hashtable.Hashtable;
+
+import java.util.HashMap;
+
+public class RepeatedWord {
+
+    public static String repeatedWord(String sentence){
+        String[] words = sentence.split(" ");
+        String temp = "";
+
+        if(words.length < 1){
+            throw new NullPointerException("empty string");
+        }
+
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        for(String word : words){
+            temp = word.toLowerCase().replaceAll("[^a-zA-Z]+$", "");
+            if(hashMap.containsKey(temp)){
+                return temp;
+            } else {
+                hashMap.put(temp, 1);
+            }
+        }
+        return null;
+    }
+}
---
+++ code401Challenges/src/test/java/code401Challenges/RepeatedWord/RepeatedWordTest.java
@@ -0,0 +1,7 @@
+package code401Challenges.RepeatedWord;
+
+import static org.junit.Assert.*;
+
+public class RepeatedWordTest {
+
+}
