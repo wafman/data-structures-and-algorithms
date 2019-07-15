c09b7c96d48eb074cc196c8ef62ad2cd2f681ae1
Will Fritts <waf515@gmail.com>
2019-07-15 13:41:58 -0700

    created  node class for graph lab

--- code401Challenges/src/main/java/code401Challenges/Graph/Node.java
+++ code401Challenges/src/main/java/code401Challenges/Graph/Node.java
@@ -1,4 +1,29 @@
 package code401Challenges.Graph;
 
+import java.util.List;
+
 public class Node {
+
+    String label;
+    private List<Node> neighbors;
+
+    public Node(String label){
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public List<Node> getNeighbors() {
+        return neighbors;
+    }
+
+    public void setNeighbors(List<Node> neighbors) {
+        this.neighbors = neighbors;
+    }
 }
